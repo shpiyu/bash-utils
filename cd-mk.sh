1939a03307652bef65ac4f8e7d0814a951552e85
Piyush Ranjan <piyushranjan95@gmail.com>
2020-06-21 13:23:58 +0530

    Simple util to replace cd with cd-mkdir

---
+++ cd-mk.sh
@@ -0,0 +1,18 @@
+dir=$1
+if [ -z $dir ]
+then
+    cd $HOME
+elif [ -d $dir ]
+then
+    cd $dir
+else
+    read -p "Directory '$dir' doesn't exist. Create and change (Y/N) " opt
+    if [ $opt == "y" ]
+    then
+        mkdir $dir
+        cd $dir
+    fi
+fi
+
+
+
