3730bc3b0ae9d196230a32552b0128da19fac71d
Piyush Ranjan <piyushranjan95@gmail.com>
2020-06-21 13:52:03 +0530

    make it a shell function

--- cd-mk.sh
+++ cd-mk.sh
@@ -1,18 +1,23 @@
-dir=$1
-if [ -z $dir ]
-then
-    cd $HOME
-elif [ -d $dir ]
-then
-    cd $dir
-else
-    read -p "Directory '$dir' doesn't exist. Create and change (Y/N) " opt
-    if [ $opt == "y" ]
+cdmk() {
+
+    dir=$1
+    if [ -z $dir ]
     then
-        mkdir $dir
+        cd $HOME
+    elif [ -d $dir ]
+    then
         cd $dir
+    else
+        read -p "Directory '$dir' doesn't exist. Create and change (Y/N) " opt
+        if [ $opt == "y" ]
+        then
+            mkdir $dir
+            cd $dir
+        fi
     fi
-fi
+}
+
+cdmk
 
 
 
